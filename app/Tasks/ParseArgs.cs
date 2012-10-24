0d1c052728a8493310fe9adb5a7dbc72593c722e
Greg Scott <i@gregoryjscott.com>
2012-10-24 12:59:00 -0800

    find nuspec files

--- app/Tasks/ParseArgs.cs
+++ app/Tasks/ParseArgs.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Simpler;
 
 namespace Tasks
@@ -13,12 +15,38 @@ namespace Tasks
         public class Output
         {
             public BumpType BumpType { get; set; }
+            public string[] AssemblyInfoFiles { get; set; }
+            public string[] NuspecFiles { get; set; }
         }
 
         public override void Execute()
         {
             var bumpType = Enum.Parse(typeof(BumpType), In.Args[0], true);
             Out.BumpType = (BumpType) bumpType;
+
+            var assemblyInfoRegEx = new Regex(@"AssemblyInfo\.cs$");
+            var assemblyInfoFiles = new List<string>();
+            for (var i = 1; i < In.Args.Length; i++)
+            {
+                var match = assemblyInfoRegEx.Match(In.Args[i]);
+                if (match.Success)
+                {
+                    assemblyInfoFiles.Add(In.Args[i]);
+                }
+            }
+            Out.AssemblyInfoFiles = assemblyInfoFiles.ToArray();
+
+            var nuspecRegEx = new Regex(@"\.nuspec$");
+            var nuspecFiles = new List<string>();
+            for (var i = 1; i < In.Args.Length; i++)
+            {
+                var match = nuspecRegEx.Match(In.Args[i]);
+                if (match.Success)
+                {
+                    nuspecFiles.Add(In.Args[i]);
+                }
+            }
+            Out.NuspecFiles = nuspecFiles.ToArray();
         }
     }
 }
--- app/Tests/Tasks/ParseArgsTest.cs
+++ app/Tests/Tasks/ParseArgsTest.cs
@@ -7,6 +7,19 @@ namespace Tests.Tasks
     [TestFixture]
     public class ParseArgsTest
     {
+        private string[] _testArgs = new[]
+                                         {
+                                             "patch",
+                                             "something",
+                                             @"C:\Somewhere\AssemblyInfo.cs",
+                                             "something",
+                                             @"C:\Somewhere\Foo.nuspec",
+                                             "something",
+                                             @"C:\SomewhereElse\AssemblyInfo.cs",
+                                             "somethingElse",
+                                             @"C:\SomewhereElse\Bar.nuspec"
+                                         };
+
         [Test]
         public void should_find_major_bump_type_in_first_arg()
         {
@@ -47,6 +60,38 @@ namespace Tests.Tasks
 
             // Assert
             Check.That(parse.Out.BumpType == BumpType.Patch, "Should have found Patch.");
+        }
+
+        [Test]
+        public void should_find_AssemblyInfo_files_in_subsequent_args()
+        {
+            // Arrange
+            var parse = Task.New<ParseArgs>();
+            parse.In.Args = _testArgs;
+
+            // Act
+            parse.Execute();
+
+            // Assert
+            Check.That(parse.Out.AssemblyInfoFiles.Length == 2, "Should have found 2 AssemblyInfo.cs files.");
+            Check.That(parse.Out.AssemblyInfoFiles[0] == @"C:\Somewhere\AssemblyInfo.cs", "Should have found first AssemblyInfo.cs file.");
+            Check.That(parse.Out.AssemblyInfoFiles[1] == @"C:\SomewhereElse\AssemblyInfo.cs", "Should have found second AssemblyInfo.cs file.");
+        }
+
+        [Test]
+        public void should_find_nuspec_files_in_subsequent_args()
+        {
+            // Arrange
+            var parse = Task.New<ParseArgs>();
+            parse.In.Args = _testArgs;
+
+            // Act
+            parse.Execute();
+
+            // Assert
+            Check.That(parse.Out.NuspecFiles.Length == 2, "Should have found 2 .nuspec files.");
+            Check.That(parse.Out.NuspecFiles[0] == @"C:\Somewhere\Foo.nuspec", "Should have found first .nuspec file.");
+            Check.That(parse.Out.NuspecFiles[1] == @"C:\SomewhereElse\Bar.nuspec", "Should have found second .nuspec file.");
         }
     }
 }
