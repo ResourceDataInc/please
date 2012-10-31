d98f5ba38a3e0721c0132bb2ece78cd50234b1e9
Greg Scott <i@gregoryjscott.com>
2012-10-31 15:34:10 -0800

    update program to bump all nuspec files

--- app/Bump/Bump/Program.cs
+++ app/Bump/Bump/Program.cs
@@ -10,6 +10,15 @@ namespace Bump
             var parse = Task.New<ParseArgs>();
             parse.In.Args = args;
             parse.Execute();
+
+            var bumpNuspec = Task.New<BumpNuspec>();
+            bumpNuspec.In.BumpType = parse.Out.BumpType;
+
+            foreach (var nuspecFile in parse.Out.NuspecFiles)
+            {
+                bumpNuspec.In.FileName = nuspecFile;
+                bumpNuspec.Execute();
+            }
         }
     }
 }
