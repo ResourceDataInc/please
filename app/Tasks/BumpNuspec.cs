747f05490fdb11fde2a70718ad8d3a6848e31844
Greg Scott <i@gregoryjscott.com>
2012-10-31 14:41:04 -0800

    implement bump nuspec task

---
+++ app/Tasks/BumpNuspec.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Simpler;
+
+namespace Tasks
+{
+    public class BumpNuspec : InTask<BumpNuspec.Input>
+    {
+        public class Input
+        {
+            public string FileName { get; set; }
+            public BumpType BumpType { get; set; }
+        }
+
+        public override void Execute()
+        {
+            var fileContents = File.ReadAllText(In.FileName);
+            const string pattern = @"<version>(?<Major>\d+)[.](?<Minor>\d+)[.](?<Patch>\d+)</version>";
+
+            var matches = Regex.Matches(fileContents, pattern);
+            Check.That(matches.Count == 1, "Expected to fine one version element in the {0} file.", In.FileName);
+            Check.That(matches[0].Groups.Count == 4, "Expected to find a version in the format of X.X.X in {0} file.", In.FileName);
+
+            var major = Int32.Parse(matches[0].Groups[1].Value);
+            var minor = Int32.Parse(matches[0].Groups[2].Value);
+            var patch = Int32.Parse(matches[0].Groups[3].Value);
+
+            switch (In.BumpType)
+            {
+                case BumpType.Major:
+                    major++;
+                    break;
+                case BumpType.Minor:
+                    minor++;
+                    break;
+                case BumpType.Patch:
+                    patch++;
+                    break;
+            }
+
+            var newVersion = String.Format(@"<version>{0}.{1}.{2}</version>", major, minor, patch);
+            var newFileContents = Regex.Replace(fileContents, pattern, match => newVersion);
+            File.WriteAllText(In.FileName, newFileContents);
+        }
+    }
+}
---
+++ app/Tests/Tasks/BumpNuspecTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using NUnit.Framework;
+using Simpler;
+using Tasks;
+
+namespace Tests.Tasks
+{
+    [TestFixture]
+    public class BumpNuspecTest
+    {
+        const string BeforeFile = @".\Files\Nuspec_Before.nuspec";
+        const string AfterFile = @".\Files\Nuspec_After.nuspec";
+        const string MajorFile = @".\Files\Nuspec_MajorBumped.nuspec";
+        const string MinorFile = @".\Files\Nuspec_MinorBumped.nuspec";
+        const string PatchFile = @".\Files\Nuspec_PatchBumped.nuspec";
+
+        static void TestBump(BumpType bumpType, string fileContainingExpectedContents)
+        {
+            // Arrange
+            File.Delete(AfterFile);
+            File.Copy(BeforeFile, AfterFile);
+
+            var bump = Task.New<BumpNuspec>();
+            bump.In.FileName = AfterFile;
+            bump.In.BumpType = bumpType;
+
+            // Act
+            bump.Execute();
+
+            // Assert
+            var afterContents = File.ReadAllText(AfterFile);
+            var expectedContents = File.ReadAllText(fileContainingExpectedContents);
+            Check.That(String.Compare(afterContents, expectedContents) == 0, "Version was not bumped correctly.");
+        }
+
+        [Test]
+        public void should_bump_major()
+        {
+            TestBump(BumpType.Major, MajorFile);
+        }
+
+        [Test]
+        public void should_bump_minor()
+        {
+            TestBump(BumpType.Minor, MinorFile);
+        }
+
+        [Test]
+        public void should_bump_patch()
+        {
+            TestBump(BumpType.Patch, PatchFile);
+        }
+    }
+}
