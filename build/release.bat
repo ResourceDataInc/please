77ecb902283200b6e2d7c94a414341dfb79c6dcd
Greg Scott <i@gregoryjscott.com>
2012-10-31 21:04:10 -0800

    fix project location

--- build/release.bat
+++ build/release.bat
@@ -1,5 +1,5 @@
 build\tools\MSBuild.exe app\Bump.sln /p:Configuration=Release
 
-build\tools\ilmerge.exe /t:winexe /out:build\output\Bump.exe /ndebug app\Bump\Bump\bin\Release\Bump.exe app\Bump\Bump\bin\Release\Castle.Core.dll app\Bump\Bump\bin\Release\Simpler.dll app\Bump\Bump\bin\Release\Library.dll 
+build\tools\ilmerge.exe /t:winexe /out:build\output\Bump.exe /ndebug app\Bump\bin\Release\Bump.exe app\Bump\bin\Release\Castle.Core.dll app\Bump\bin\Release\Simpler.dll app\Bump\bin\Release\Library.dll 
 
 exit /B %ERRORLEVEL%
