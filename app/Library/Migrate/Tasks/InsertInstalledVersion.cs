f482caf7a4ff6a68d6f5c137eb630dd189630890
Greg Scott <i@gregoryjscott.com>
2012-12-12 12:16:18 -0900

    implement insert version

---
+++ app/Library/Migrate/Tasks/InsertInstalledVersion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Simpler;
+using Simpler.Data;
+
+namespace Library.Migrate.Tasks
+{
+    public class InsertInstalledVersion : InOutTask<InsertInstalledVersion.Input, InsertInstalledVersion.Output>
+    {
+        public class Input
+        {
+            public string ConnectionName { get; set; }
+            public Version Version { get; set; }
+        }
+
+        public class Output
+        {
+            public int RowsAffected { get; set; }
+        }
+
+        public override void Execute()
+        {
+            var sql = @"INSERT INTO schema_migrations (version) VALUES ({0});";
+
+            sql = String.Format(sql, In.Version.Id);
+
+            using (var connection = Db.Connect(In.ConnectionName))
+            {
+                Out.RowsAffected = Db.GetResult(connection, sql);
+            }
+        }
+    }
+}
---
+++ app/Tests/Migrate/Tasks/InsertInstalledVersionTest.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Library.Migrate;
+using Library.Migrate.Tasks;
+using NUnit.Framework;
+using Simpler;
+
+namespace Tests.Migrate.Tasks
+{
+    [TestFixture]
+    public class InsertInstalledVersionTest
+    {
+        [Test]
+        public void should_insert_version()
+        {
+            // Arrange
+            File.Delete(@"Migrate\Files\test.db");
+            File.Copy(@"Migrate\Files\empty.db", @"Migrate\Files\test.db");
+
+            var createVersionTable = Task.New<CreateVersionTable>();
+            createVersionTable.In.ConnectionName = "Test";
+            createVersionTable.Execute();
+
+            var insertInstalledVersion = Task.New<InsertInstalledVersion>();
+            insertInstalledVersion.In.ConnectionName = "Test";
+            insertInstalledVersion.In.Version = new Version {Id = "999"};
+
+            // Act
+            insertInstalledVersion.Execute();
+
+            // Assert
+            Check.That(insertInstalledVersion.Out.RowsAffected == 1, "Expected to insert 1 version.");
+        }
+    }
+}
