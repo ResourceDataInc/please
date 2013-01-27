130ca8350236d6b3ada473c21c7662bb7002d8a9
Greg Scott <i@gregoryjscott.com>
2013-01-26 18:59:27 -0900

    refactor please tests

--- app/Tests/PleaseTests.cs
+++ app/Tests/PleaseTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Library;
 using Library.Bump;
 using Library.Bump.Tasks;
@@ -10,118 +11,78 @@ namespace Tests
     [TestFixture]
     public class PleaseTests
     {
+        static BumpFile PleaseBump(string options)
+        {
+            var please = Task.New<Please>();
+            please.In.Args = ("bump " + options).Split(' ');
+
+            var bumpCommand = Commands.All.Single(c => c.Name == "bump");
+            bumpCommand.Task = Fake.Task<BumpFile>();
+
+            please.Execute();
+
+            var bumpFile = bumpCommand.Task as BumpFile;
+            if (bumpFile == null) throw new Exception();
+            return bumpFile;
+        }
+
+        static Library.RunSql.Tasks.RunSql PleaseRunSql(string options)
+        {
+            var please = Task.New<Please>();
+            please.In.Args = ("run sql " + options).Split(' ');
+
+            var runSqlCommand = Commands.All.Single(c => c.Name == "run sql");
+            runSqlCommand.Task = Fake.Task<Library.RunSql.Tasks.RunSql>();
+
+            please.Execute();
+
+            var runSql = runSqlCommand.Task as Library.RunSql.Tasks.RunSql;
+            if (runSql == null) throw new Exception();
+            return runSql;
+        }
+
         [Test]
         public void should_bump_major_in_AssemblyInfo()
         {
-            // Arrange
-            var please = Task.New<Please>();
-            please.In.Args = "bump major in AssemblyInfo.cs".Split(' ');
+            var bumpFile = PleaseBump("major in AssemblyInfo.cs");
 
-            var bumpType = BumpType.Unknown;
-            var fileType = FileType.Unknown;
-            var bumpCommand = Commands.All.Single(c => c.Name == "bump");
-            bumpCommand.Task = Fake.Task<BumpFile>(bf =>
-            {
-                bumpType = bf.In.BumpType;
-                fileType = bf.In.FileType;
-            });
-
-            // Act
-            please.Execute();
-
-            // Assert
-            Check.That(bumpType == BumpType.Major, "Expected bump type to be major.");
-            Check.That(fileType == FileType.AssemblyInfo, "Expected file type to be AssemblyInfo.");
+            Check.That(bumpFile.In.BumpType == BumpType.Major, "Expected bump type to be major.");
+            Check.That(bumpFile.In.FileType == FileType.AssemblyInfo, "Expected file type to be AssemblyInfo.");
         }
 
         [Test]
         public void should_bump_minor_in_nuspec()
         {
-            // Arrange
-            var please = Task.New<Please>();
-            please.In.Args = "bump minor in Something.nuspec".Split(' ');
+            var bumpFile = PleaseBump("minor in Something.nuspec");
 
-            var bumpType = BumpType.Unknown;
-            var fileType = FileType.Unknown;
-            var bumpCommand = Commands.All.Single(c => c.Name == "bump");
-            bumpCommand.Task = Fake.Task<BumpFile>(bf =>
-            {
-                bumpType = bf.In.BumpType;
-                fileType = bf.In.FileType;
-            });
-
-            // Act
-            please.Execute();
-
-            // Assert
-            Check.That(bumpType == BumpType.Minor, "Expected bump type to be minor.");
-            Check.That(fileType == FileType.Nuspec, "Expected file type to be nuspec.");
+            Check.That(bumpFile.In.BumpType == BumpType.Minor, "Expected bump type to be minor.");
+            Check.That(bumpFile.In.FileType == FileType.Nuspec, "Expected file type to be nuspec.");
         }
 
         [Test]
         public void should_bump_patch_in_script()
         {
-            // Arrange
-            var please = Task.New<Please>();
-            please.In.Args = "bump patch in Whatever.bat".Split(' ');
+            var bumpFile = PleaseBump("patch in Whatever.bat");
 
-            var bumpType = BumpType.Unknown;
-            var fileType = FileType.Unknown;
-            var bumpCommand = Commands.All.Single(c => c.Name == "bump");
-            bumpCommand.Task = Fake.Task<BumpFile>(bf =>
-            {
-                bumpType = bf.In.BumpType;
-                fileType = bf.In.FileType;
-            });
- 
-            // Act
-            please.Execute();
-
-            // Assert
-            Check.That(bumpType == BumpType.Patch, "Expected bump type to be patch.");
-            Check.That(fileType == FileType.Script, "Expected file type to be script.");
+            Check.That(bumpFile.In.BumpType == BumpType.Patch, "Expected bump type to be patch.");
+            Check.That(bumpFile.In.FileType == FileType.Script, "Expected file type to be script.");
         }
 
         [Test]
         public void should_run_sql_on_database_in_directory()
         {
-            // Arrange
-            var please = Task.New<Please>();
-            please.In.Args = "run sql on DEV in SomeDirectory".Split(' ');
+            var runSql = PleaseRunSql("on DEV in SomeDirectory");
 
-            var database = "";
-            var directory = "";
-            var runSqlCommand = Commands.All.Single(c => c.Name == "run sql");
-            runSqlCommand.Task = Fake.Task<Library.RunSql.Tasks.RunSql>(rs =>
-                                                       {
-                                                           database = rs.In.Args[0];
-                                                           directory = rs.In.Args[1];
-                                                       });
-
-            // Act
-            please.Execute();
-
-            // Assert
-            Check.That(database == "DEV", "Expected database to be DEV.");
-            Check.That(directory == "SomeDirectory", "Expected directory to be SomeDirectory.");
+            Check.That(runSql.In.Args[0] == "DEV", "Expected database to be DEV.");
+            Check.That(runSql.In.Args[1] == "SomeDirectory", "Expected directory to be SomeDirectory.");
         }
 
         [Test]
         public void should_run_sql_with_versioning()
         {
-            // Arrange
-            var please = Task.New<Please>();
-            please.In.Args = "run sql with versioning".Split(' ');
+            var runSql = PleaseRunSql("with versioning");
 
-            var withVersioning = false;
-            var runSqlCommand = Commands.All.Single(c => c.Name == "run sql");
-            runSqlCommand.Task = Fake.Task<Library.RunSql.Tasks.RunSql>(rs => withVersioning = rs.In.WithVersioning);
-
-            // Act
-            please.Execute();
-
-            // Assert
-            Check.That(withVersioning, "Expected with versioning to be set to true.");
+            Check.That(runSql.In.WithVersioning, "Expected with versioning to be set to true.");
         }
     }
 }
