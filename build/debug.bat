74024a2c4c1ee412f364cfa67e1b580a02759142
Greg Scott <i@gregoryjscott.com>
2014-05-06 11:30:08 -0800

    Delete old scripts and tools

--- build/debug.bat
+++
@@ -1,3 +0,0 @@
-build\tools\MSBuild.exe app\Please.sln /p:Configuration=Debug
-
-exit /B %ERRORLEVEL%
--- release/bump.bat
+++
@@ -1,3 +0,0 @@
-release\tools\please.exe bump %1 version in app\Library\Properties\AssemblyInfo.cs
-release\tools\please.exe bump %1 version in app\Please\Properties\AssemblyInfo.cs
-release\tools\please.exe bump %1 version in app\Tests\Properties\AssemblyInfo.cs
